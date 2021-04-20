76e8f185319987f4408ffdfde5c77401d14478ef
YutaMizusawa <yuta.mizusawa@dive.design>
2021-04-20 13:26:11 +0900

    ngIfとsubmitを使い、ビューからデータを渡す処理を実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { Comment } from "./class/chat";
+import { Comment, User } from "./class/chat";
 
+const CURRENT_USER: User = new User(1, 'Tanaka Jiro');
+const ANOTHER_USER: User = new User(2, 'Suzuki Taro');
 const COMMENTS: Comment[] = [
-  { name: 'Suzuki Taro',  content: '１つ目のコメントです。'},
-  { name: 'Suzuki Taro',  content: '２つ目のコメントです。'},
-  { name: 'Suzuki Taro',  content: '３つ目のコメントです。'}
+  new Comment(ANOTHER_USER, 'Suzukiの1つ目のコメントです。'),
+  new Comment(ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
+  new Comment(CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
+  new Comment(ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
+  new Comment(CURRENT_USER, 'Tanakaの2つ目のコメントです。')
 ];
 
 @Component({
@@ -15,4 +19,11 @@ const COMMENTS: Comment[] = [
 export class AppComponent {
   public content = '';
   public comments = COMMENTS;
+  public current_user = CURRENT_USER;
+
+  addComment(comment: string) {
+    if (comment) {
+      this.comments.push(new Comment(this.current_user, comment));
+    }
+  }
 }
