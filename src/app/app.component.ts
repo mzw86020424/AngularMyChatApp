757f8795271a0dc9aa4e3a2f1264484842bd1485
yuta.mizusawa <yuta.mizusawa@apptli.local>
2021-04-21 19:19:07 +0900

    Angular+Cloud FirestoreでCRUDを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,11 @@ import { Component } from '@angular/core';
 import { Comment, User } from "./class/chat";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 const CURRENT_USER: User = new User(1, 'Tanaka Jiro');
 const ANOTHER_USER: User = new User(2, 'Suzuki Taro');
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'Suzukiの1つ目のコメントです。'),
-  new Comment(ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
-  new Comment(CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
-  new Comment(ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
-  new Comment(CURRENT_USER, 'Tanakaの2つ目のコメントです。')
-];
+
 
 @Component({
   selector: 'app-root',
@@ -19,21 +14,68 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  item: Observable<Comment>;
   public content = '';
-  public comments = COMMENTS;
-  public current_user = CURRENT_USER;
+  public comments: Observable<Comment[]>;
+  public currentUser = CURRENT_USER;
 
-  constructor(db: AngularFirestore) {
-    this.item = db
-      .collection('comments')
-      .doc<Comment>('item')
-      .valueChanges();
+  // DI（依存性注入する機能を指定）
+  constructor(private db: AngularFirestore) {
+    this.comments = db
+      .collection<Comment>('comments', ref => {
+        return ref.orderBy('date', 'asc');
+      })
+      .snapshotChanges()
+      .pipe(
+        map(actions => actions.map(action => {
+          // 日付をセットしたコメントを返す
+          const data = action.payload.doc.data() as Comment;
+          const key = action.payload.doc.id;
+          const commentData = new Comment(data.user, data.content);
+          commentData.setData(data.date, key);
+          return commentData;
+        }))
+      );
+      console.log(this.comments);
   }
 
-  addComment(comment: string) {
+  addComment(e: Event, comment: string) {
     if (comment) {
-      this.comments.push(new Comment(this.current_user, comment));
+      this.db
+        .collection('comments')
+        .add(new Comment(this.currentUser, comment).deserialize());
+      this.content = '';
     }
   }
+
+  toggleEditComment(comment: Comment) {
+    comment.editFlag = (!comment.editFlag)
+  }
+
+  saveEditComment(comment: Comment) {
+    this.db
+      .collection('comments')
+      .doc(comment.key)
+      .update({
+        content: comment.content,
+        date: comment.date,
+      })
+      .then(()=> {
+        alert('コメントを更新しました');
+        comment.editFlag = false;
+      })
+  };
+
+  resetEditComment(comment: Comment) {
+    comment.content = '';
+  }
+
+  deleteComment(key: string) {
+    this.db
+     .collection('comments')
+     .doc(key)
+     .delete()
+     .then(() => {
+       alert(',コメントを削除しました');
+     })
+  };
 }
