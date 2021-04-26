4ba4e84be0d87211a6756a5b0b277384fd66406a
YutaMizusawa <yuta.mizusawa@dive.design>
2021-04-26 12:10:49 +0900

    ルーティング設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { SharedModule } from './shared/shared.module'; // 追加
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    NgbModule, // .forRoot(),
     BrowserModule,
+    RouterModule,
+    AppRoutingModule,
     SharedModule,
+    NgbModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
@@ -28,4 +34,5 @@ import { HeaderComponent } from './header/header.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
